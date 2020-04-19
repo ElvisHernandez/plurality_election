db3b1871c2abc84a5716ddff2cd3312e6e9e93d2
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-19 18:17:19 -0400

    added merge sort implementation

--- cs50_pset3P1.c
+++ cs50_pset3P1.c
@@ -22,6 +22,9 @@ int candidate_count;
 // Function prototypes
 bool vote(string name);
 void print_winner(void);
+bool compare_strings(string one, string two);
+int merge_sort(int nums[], int nums_length);
+void merge_halfs(int nums[],int la[], int la_len, int ra[], int ra_len);
 
 int main(int argc, string argv[])
 {
@@ -66,15 +69,92 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++) {
+        if (compare_strings(candidates[i].name,name)) {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+    int votes[candidate_count];
+
+    for (int i = 0; i < candidate_count; i++) {
+        votes[i] = candidates[i].votes;
+    }
+    merge_sort(votes,candidate_count);
+    int max_votes = votes[candidate_count-1];
+
+    for (int i = 0; i < candidate_count; i++) {
+        if (candidates[i].votes == max_votes) {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+
     // TODO
     return;
 }
 
+bool compare_strings(string one, string two) {
+    for (int i = 0; one[i] != '\0'; i++) {
+        if (one[i] != two[i]) return false;
+    }
+    for (int i = 0; two[i] != '\0'; i++) {
+        if (two[i] != one[i]) return false;
+    }
+    return true;
+}
 
+int merge_sort(int nums[], int nums_length) {
+    if (nums_length == 1)
+        return 0;
+    int mid = nums_length/2;
+    int la[mid];
+    int ra[nums_length-mid];
+    for (int i = 0; i < mid; i++) {
+        la[i] = nums[i];
+    }
+    for (int i = 0; i < nums_length-mid; i++) {
+        ra[i] = nums[mid+i];
+    }
+    merge_sort(la, mid);
+    merge_sort(ra,nums_length-mid);
+    merge_halfs(nums,la,mid,ra, nums_length-mid);
+    return 0;
+}
+
+void merge_halfs(int nums[],int la[], int la_len, int ra[], int ra_len) {
+    int merged_array[la_len + ra_len];
+    int la_i = 0;
+    int ra_i = 0; // these are the left, right, and merged arrays respectively
+    int ma_i = 0;
+
+    while (la_i < la_len && ra_i < ra_len) {
+        if (la[la_i] < ra[ra_i]) {
+            merged_array[ma_i] = la[la_i];
+            la_i++;
+        } else {
+            merged_array[ma_i] = ra[ra_i];
+            ra_i++;
+        }
+        ma_i++;
+    }
+    while (la_i < la_len) {
+        merged_array[ma_i] = la[la_i];
+        la_i++;
+        ma_i++;
+    }
+    while (ra_i < ra_len) {
+        merged_array[ma_i] = ra[ra_i];
+        ra_i++;
+        ma_i++;
+    }
+    for (int i = 0; i < (la_len + ra_len); i++) {
+        nums[i] = merged_array[i];
+        //printf("Merged array element %i: %i\n", i+1,merged_array[i]);
+    }
+}
